1224033a7d6a79b068af294d38ccd4d1f5ea3ad6
ItsMeAadityaK <80200537+ItsMeAadityaK@users.noreply.github.com>
2025-05-08 16:45:26 +1000

    navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import UseEffectEx from './UseEffectEx';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import Profile from './Profile';
 
 function App() {
   return (
-    <div className="App">
-      <UseEffectEx />
-    </div>
+    <Router>
+      <nav style={{ padding: '1rem' }}>
+        <Link to="/" style={{ marginRight: '1rem' }}>Home</Link>
+        <Link to="/profile">Profile</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
   );
 }
 
